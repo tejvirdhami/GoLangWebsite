fa13b69c6f3599dfcdaa006a2d6970e0f12e9136
Tejvir Dhami <62151509+tejvirdhami@users.noreply.github.com>
2022-03-24 14:35:47 -0400

    first commit

---
+++ main.go
@@ -0,0 +1,120 @@
+package main
+
+import (
+	"encoding/json"
+	"html/template"
+	"log"
+	"myapp/util"
+	"net/http"
+)
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "index.html")
+}
+
+// func ipPage(w http.ResponseWriter, r *http.Request) {
+// 	renderTemplate(w, "ipValidator.html")
+// }
+
+func playLottery(w http.ResponseWriter, r *http.Request) {
+	//i have added here playerChoice variable
+	//from object request, we getting variable "c" content
+	result := util.PlayLottery()
+
+	out, err := json.MarshalIndent(result, "", "    ")
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(out)
+}
+
+func validateIp(w http.ResponseWriter, r *http.Request) {
+	//i have added here playerChoice variable
+	//from object request, we getting variable "c" content
+	ip := (r.URL.Query().Get("c"))
+	result := util.ValidateIp(string(ip))
+
+	out, err := json.MarshalIndent(result, "", "    ")
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(out)
+}
+
+func getWeather(w http.ResponseWriter, r *http.Request) {
+	//i have added here playerChoice variable
+	//from object request, we getting variable "c" content
+	city := (r.URL.Query().Get("c"))
+	result := util.GetWeather(string(city))
+
+	out, err := json.MarshalIndent(result, "", "    ")
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(out)
+}
+
+func convertCurrency(w http.ResponseWriter, r *http.Request) {
+	//i have added here playerChoice variable
+	//from object request, we getting variable "c" content
+	from := (r.URL.Query().Get("from"))
+	to := (r.URL.Query().Get("to"))
+	have := (r.URL.Query().Get("have"))
+	result := util.ConvertCurrency(string(from), string(to), string(have))
+
+	out, err := json.MarshalIndent(result, "", "    ")
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(out)
+}
+
+func sendSms(w http.ResponseWriter, r *http.Request) {
+	//i have added here playerChoice variable
+	//from object request, we getting variable "c" content
+	number := (r.URL.Query().Get("number"))
+	message := (r.URL.Query().Get("message"))
+	result := util.SendSms(string(number), string(message))
+
+	out, err := json.MarshalIndent(result, "", "    ")
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(out)
+}
+
+func main() {
+	http.HandleFunc("/play", playLottery)
+	http.HandleFunc("/verify", validateIp)
+	http.HandleFunc("/getweather", getWeather)
+	http.HandleFunc("/convertCurrency", convertCurrency)
+	http.HandleFunc("/sendSms", sendSms)
+	http.HandleFunc("/", homePage)
+
+	log.Println("Starting web server on port 8080")
+	http.ListenAndServe(":8080", nil)
+}
+
+func renderTemplate(w http.ResponseWriter, page string) {
+	t, err := template.ParseFiles(page)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	err = t.Execute(w, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+}
---
+++ util/util.go
@@ -0,0 +1,141 @@
+package util
+
+import (
+	"io/ioutil"
+	"math/rand"
+	"net"
+	"net/http"
+	"strconv"
+	"strings"
+)
+
+type Round struct {
+	WinningNumbers    string `json:"winning_numbers"`
+	IpValidatorResult string `json:"ip_validator_result"`
+	WeatherReport     string `json:"weather_report"`
+	ConvertedCurrency string `json:"converted_currency"`
+	MessageStatus     string `json:"message_status"`
+}
+
+func PlayLottery() Round {
+	var nums []int
+	var min int = 0
+	var max int = 50
+	var i int = 0
+	winningNumbers := ""
+	var randomNumber int = 0
+
+	for i = 0; i < 8; i++ {
+		randomNumber = (rand.Intn(max-min) + min)
+		nums = append(nums, randomNumber)
+	}
+
+	winningNumbers = strconv.Itoa(nums[0])
+
+	for i = 1; i < 8; i++ {
+		winningNumbers = winningNumbers + ", " + strconv.Itoa(nums[i])
+	}
+
+	var result Round
+	result.WinningNumbers = winningNumbers
+	return result
+
+}
+
+func ValidateIp(ip string) Round {
+	ipValidatorResult := ""
+
+	//--------------JUST FOUND OUT THIS LIBRARY IN GO THAT AUTOMATICALLY CHECKS IP WITHOUT USING REGEX!-------------
+
+	if net.ParseIP(ip) != nil {
+		ipValidatorResult = "It is a Valid IP!"
+	} else {
+		ipValidatorResult = "It is an Invalid IP!"
+	}
+
+	// error := ""
+	// obj, err := regexp.Match("(([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\\.){3}([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])", []byte(ip))
+	// if obj == true {
+	// 	ipValidatorResult = "It is a Valid IP!"
+	// } else if obj == false {
+	// 	ipValidatorResult = "It is an Invalid IP!"
+	// } else {
+	// 	error = err.Error()
+	// 	fmt.Print(error)
+	// }
+
+	var result Round
+	result.IpValidatorResult = ipValidatorResult
+	return result
+}
+
+func GetWeather(city string) Round {
+
+	url := "https://weatherapi-com.p.rapidapi.com/current.json?q=" + city
+
+	req, _ := http.NewRequest("GET", url, nil)
+
+	req.Header.Add("x-rapidapi-host", "weatherapi-com.p.rapidapi.com")
+	req.Header.Add("x-rapidapi-key", "50816c55bemsha74515a42a5acf6p160902jsn8da02f65d690")
+
+	res, _ := http.DefaultClient.Do(req)
+
+	defer res.Body.Close()
+	body, _ := ioutil.ReadAll(res.Body)
+
+	// fmt.Println(res)
+	// fmt.Println(string(body))
+
+	var result Round
+	result.WeatherReport = (string(body))
+	return result
+
+}
+
+func ConvertCurrency(from string, to string, have string) Round {
+	url := "https://currency-converter-by-api-ninjas.p.rapidapi.com/v1/convertcurrency?have=" + from + "&want=" + to + "&amount=" + have
+
+	req, _ := http.NewRequest("GET", url, nil)
+
+	req.Header.Add("x-rapidapi-host", "currency-converter-by-api-ninjas.p.rapidapi.com")
+	req.Header.Add("x-rapidapi-key", "50816c55bemsha74515a42a5acf6p160902jsn8da02f65d690")
+
+	res, _ := http.DefaultClient.Do(req)
+
+	defer res.Body.Close()
+	body, _ := ioutil.ReadAll(res.Body)
+
+	// fmt.Println(res)
+	// fmt.Println(string(body))
+
+	var result Round
+	result.ConvertedCurrency = (string(body))
+	return result
+}
+
+func SendSms(number string, text string) Round {
+
+	url := "https://sms77io.p.rapidapi.com/sms"
+
+	payload := strings.NewReader("to=%2B" + number + "&p=LltHlrQFTmAyaODZ8upB7jQVlNpikxg2x0fZYRuVSzMBClV0bnfXvmnUdRDAu7f3&text=" + text + "")
+
+	req, _ := http.NewRequest("POST", url, payload)
+
+	req.Header.Add("content-type", "application/x-www-form-urlencoded")
+	req.Header.Add("X-RapidAPI-Host", "sms77io.p.rapidapi.com")
+	req.Header.Add("X-RapidAPI-Key", "50816c55bemsha74515a42a5acf6p160902jsn8da02f65d690")
+
+	res, _ := http.DefaultClient.Do(req)
+
+	defer res.Body.Close()
+	body, _ := ioutil.ReadAll(res.Body)
+
+	// fmt.Println(res)
+	// fmt.Println(string(body))
+
+	var result Round
+	// result.MessageStatus = "Message Sent!"
+	result.MessageStatus = (string(body))
+	return result
+
+}
